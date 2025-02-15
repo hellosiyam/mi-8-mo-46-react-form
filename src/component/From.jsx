4928e8ba96c6b9f247d582aabb0a2961793579f6
hellosiyam <hellosiyambd@gmail.com>
2025-02-15 20:11:57 +0600

    mi-8-mo-46-react-form

---
+++ src/component/From.jsx
@@ -0,0 +1,28 @@
+
+const From = () => {
+
+    const submitHandel = e => {
+        e.preventDefault()
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log(e.target.phone.value);
+        console.log('from submitted');
+        
+    }
+
+    return (
+        <div>
+            <form onSubmit={submitHandel}>
+                <input type="text" name="name"/>
+                <br />
+                <input type="email" name="email"/>
+                <br />
+                <input type="text" name="phone"/>
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default From;
